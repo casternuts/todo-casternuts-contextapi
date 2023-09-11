608fc2d5f05ad61eb885f5b31040f01fc7b66410
홍영기 그린푸드 <2012188@hyundaidept.com>
2023-09-11 15:32:36 +0900

    리듀스 버전 적용

--- src/AppReduce.js
+++ src/AppReduce.js
@@ -1,8 +1,7 @@
-
-import './App.css';
-import { React, useEffect, useState } from 'react';
-import { useTodoState, useTodoDispatch } from './context/TodoReduceContext';
-import { TodoList } from './components/TodoListComp'
+import "./App.css";
+import { React, useEffect, useState } from "react";
+import { useTodoState, useTodoDispatch } from "./context/TodoReduceContext";
+import { TodoList } from "./components/TodoListComp";
 
 function App() {
   const [isCompleteScreen, setIsCompleteScreen] = useState(false);
@@ -11,29 +10,27 @@ function App() {
   const state = useTodoState();
   const dispatch = useTodoDispatch();
 
-
   //추가
   const handleAddTodo = () => {
-    console.log('ddd')
+    console.log("ddd");
     let newTodoItem = {
       title: newTitle,
-      description: newDescription
-    }
-    dispatch({ type: 'SET_TODOS', payload: newTodoItem });
-  }
+      description: newDescription,
+    };
+    dispatch({ type: "SET_TODOS", payload: newTodoItem });
+  };
   //삭제
   const handleDeleteTodo = (index) => {
-    dispatch({ type: 'DELETE_TODOS', payload: index });
+    dispatch({ type: "DELETE_TODOS", payload: index });
   };
   //삭제(완료한일)
   const handleDeleteCompleteTodo = (index) => {
     console.log(index);
-    dispatch({ type: 'DELETE_COMP_TODOS', payload: index });
-
+    dispatch({ type: "DELETE_COMP_TODOS", payload: index });
   };
   //완료
   const handleComplete = (index) => {
-    console.log(index)
+    console.log(index);
     let now = new Date();
     let dd = now.getDate();
     let mm = now.getMonth() + 1;
@@ -41,64 +38,96 @@ function App() {
     let h = now.getHours();
     let m = now.getMinutes();
     let s = now.getSeconds();
-    let completedOn = dd + '-' + mm + '-' + yyyy + ' at ' + h + ':' + m + ':' + s;
+    let completedOn =
+      dd + "-" + mm + "-" + yyyy + " at " + h + ":" + m + ":" + s;
     let filteredItem = {
       ...state.allTodos[index],
-      completedOn: completedOn
-    }
-    dispatch({ type: 'SET_COMP_TODOS', payload: filteredItem });
+      completedOn: completedOn,
+    };
+    dispatch({ type: "SET_COMP_TODOS", payload: filteredItem });
 
-    handleDeleteTodo(index)
-  }
+    handleDeleteTodo(index);
+  };
 
   // 초기 데이터 로딩
-  useEffect(() => {
-    let savedTodo = JSON.parse(localStorage.getItem('todolist'));
-    let savedCompletedTodo = JSON.parse(localStorage.getItem('completedTodos'));
-    if (savedTodo) {
-      dispatch({ type: 'SET_INIT_TODOS', payload: savedTodo });
-    }
-    if (savedCompletedTodo) {
-      dispatch({ type: 'SET_INIT_COMP_TODOS', payload: savedCompletedTodo });
-    }
-  }, [dispatch])
-
+  useEffect(
+    () => {
+      console.log("초기로딩");
+      let savedTodo = JSON.parse(localStorage.getItem("todolist"));
+      let savedCompletedTodo = JSON.parse(
+        localStorage.getItem("completedTodos")
+      );
+      if (savedTodo) {
+        dispatch({ type: "SET_INIT_TODOS", payload: savedTodo });
+      }
+      if (savedCompletedTodo) {
+        dispatch({ type: "SET_INIT_COMP_TODOS", payload: savedCompletedTodo });
+      }
+    },
+    // eslint-disable-next-line
+    []
+  );
 
   return (
-
     <div className="App">
       <h1>나의 할일</h1>
       <div className="todo-wrapper">
         <div className="todo-input">
           <div className="todo-input-item">
             <label>Title</label>
-            <input type="text" value={newTitle} onChange={(e) => setNewTitle(e.target.value)} placeholder="제목"></input>
+            <input
+              type="text"
+              value={newTitle}
+              onChange={(e) => setNewTitle(e.target.value)}
+              placeholder="제목"
+            ></input>
           </div>
           <div className="todo-input-item">
             <label>설명</label>
-            <input type="text" placeholder="설명" value={newDescription} onChange={(e) => setNewDescription(e.target.value)}></input>
+            <input
+              type="text"
+              placeholder="설명"
+              value={newDescription}
+              onChange={(e) => setNewDescription(e.target.value)}
+            ></input>
           </div>
           <div className="todo-input-item">
-            <button type="button" onClick={handleAddTodo} className="primaryBtn">추가</button>
+            <button
+              type="button"
+              onClick={handleAddTodo}
+              className="primaryBtn"
+            >
+              추가
+            </button>
           </div>
         </div>
 
         <div className="btn-area">
-          <button type="button" className={`secondaryBtn ${isCompleteScreen === false && 'active'}`} onClick={() => setIsCompleteScreen(false)}>해야할일</button>
-          <button type="button" className={`secondaryBtn ${isCompleteScreen === true && 'active'}`} onClick={() => setIsCompleteScreen(true)}>완료</button>
+          <button
+            type="button"
+            className={`secondaryBtn ${isCompleteScreen === false && "active"}`}
+            onClick={() => setIsCompleteScreen(false)}
+          >
+            해야할일
+          </button>
+          <button
+            type="button"
+            className={`secondaryBtn ${isCompleteScreen === true && "active"}`}
+            onClick={() => setIsCompleteScreen(true)}
+          >
+            완료
+          </button>
         </div>
 
-
-
-        <TodoList isCompleteScreen={isCompleteScreen} handleDeleteTodo={handleDeleteTodo} handleComplete={handleComplete} handleDeleteCompleteTodo={handleDeleteCompleteTodo} ></TodoList>
-
-
-
+        <TodoList
+          isCompleteScreen={isCompleteScreen}
+          handleDeleteTodo={handleDeleteTodo}
+          handleComplete={handleComplete}
+          handleDeleteCompleteTodo={handleDeleteCompleteTodo}
+        ></TodoList>
       </div>
-    </div >
-
+    </div>
   );
 }
 
 export default App;
-
